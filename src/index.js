10d7b6528d4bad3e5d5bfe2b4d38c44a4c5b3804
Mikhail Rohau <misha_rogov@tut.by>
2018-02-26 20:55:16 +0300

    Creating money exchange function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let obj = new Object();
+    if (currency <= 0) {
+        return obj;
+    } else if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    } else {
+        getNumberOfCoins(currency, obj);
+        return obj;
+    }
+}
+function getNumberOfCoins(cur, obj) {console.log(obj);
+    if (cur >= 50) {
+        obj.H = Math.trunc(cur / 50);
+        getNumberOfCoins(cur % 50, obj);
+        return;
+    }
+    if (cur >= 25) {
+        obj.Q = Math.trunc(cur / 25);
+        getNumberOfCoins(cur % 25, obj);
+        return;
+    }
+
+    if (cur >= 10) {
+        obj.D = Math.trunc(cur / 10);
+        getNumberOfCoins(cur % 10, obj);
+        return;
+    }
+    if (cur >= 5) {
+        obj.N = Math.trunc(cur / 5);
+        getNumberOfCoins(cur % 5, obj);
+        return;
+    }
+    if (cur >= 1) {
+        console.log(cur);
+        obj.P = cur;
+        console.log(obj);
+    }
 }
